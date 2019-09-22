d82834005a7196d6d1351bd843e5c15ca1155889
pornnipaj <pornniparj@gmail.com>
2019-09-22 18:01:38 +0700

    add

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(public navCtrl:NavController) { }
+
+  ngOnInit() {
+  }
+
+
+  click(){
+    this.navCtrl.navigateForward(['/profile']);
+  }
+
+}
